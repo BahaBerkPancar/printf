15c8f040d826927849090aeed814f2066b5236b3
BahaBerkPancar <bahaberkpancar@gmail.com>
2024-10-27 17:22:50 +0300

    Add files via upload

---
+++ printf/printfc/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpancar <bpancar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 17:16:51 by bpancar           #+#    #+#             */
+/*   Updated: 2024/10/27 17:17:29 by bpancar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_C
+# define FT_PRINTF_C
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_check_type(char c, va_list arg);
+int	ft_printf(const char *str, ...);
+int	ft_putnbr(int n);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_printf_unsigned(unsigned int a);
+int	ft_hexa(unsigned int i, char c);
+int	ft_pointer(unsigned long p);
+
+#endif
